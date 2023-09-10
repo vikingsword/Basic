22b1906600a7f065879d7d6c30f43770517cb030
vikingar <423141759@qq.com>
2023-09-10 22:28:04 +0800

    practice

--- p84/07_operators_reload.cpp
+++ p84/07_operators_reload.cpp
@@ -1,404 +1,404 @@
-#include "iostream"
-
-using namespace std;
-
-class Person {
-
-public:
-    int m_a;
-    int m_b;
-
-public:
-    int add_param() {
-        return m_a + m_b;
-    }
-
-    Person add_person_param(Person &p) {
-        Person temp;
-        temp.m_a = this->m_a + p.m_a;
-        temp.m_b = this->m_b + p.m_b;
-        return temp;
-    }
-
-    Person add_person_param2(Person p) {
-        Person temp;
-        temp.m_a = this->m_a + p.m_a;
-        temp.m_b = this->m_b + p.m_b;
-        return temp;
-    }
-
-//    Person operator+(Person &p) {
+//#include "iostream"
+//
+//using namespace std;
+//
+//class Person {
+//
+//public:
+//    int m_a;
+//    int m_b;
+//
+//public:
+//    int add_param() {
+//        return m_a + m_b;
+//    }
+//
+//    Person add_person_param(Person &p) {
 //        Person temp;
 //        temp.m_a = this->m_a + p.m_a;
 //        temp.m_b = this->m_b + p.m_b;
 //        return temp;
 //    }
-
-};
-
-
-Person operator+(Person &p1, Person &p2) {
-    Person temp;
-    temp.m_a = p1.m_a + p2.m_a;
-    temp.m_b = p1.m_b + p2.m_b;
-    return temp;
-}
-
-// operator+ 重载
-Person operator+(Person &p1, int num) {
-    Person temp;
-    temp.m_a = p1.m_a + num;
-    temp.m_b = p1.m_b + num;
-    return temp;
-}
-
-Person operator-(Person &p1, Person &p2) {
-    Person temp;
-    temp.m_a = p1.m_a - p2.m_a;
-    temp.m_b = p1.m_b - p2.m_b;
-    return temp;
-}
-
-Person operator*(Person &p1, Person &p2) {
-    Person temp;
-    temp.m_a = p1.m_a * p2.m_a;
-    temp.m_b = p1.m_b * p2.m_b;
-    return temp;
-}
-
-void test01() {
-
-    Person p1;
-    p1.m_a = 10;
-    p1.m_b = 10;
-
-    Person p2;
-    p2.m_a = 10;
-    p2.m_b = 10;
-
-//    p1.add_person_param(p2);
-//    cout << "p1.m_a = " << p1.m_a << endl;
-//    cout << "p2.m_a = " << p2.m_a << endl;
-
-//    p1.add_person_param2(p2);
-//    cout << "p1.m_a = " << p1.m_a << endl;
-//    cout << "p2.m_a = " << p2.m_a << endl;
-
-//    Person p3 = p1.operator+(p2);
-//    cout << "p3.m_a = " << p3.m_a << endl;
-
-    // 通过成员函数重载 + 号
-//    Person p4 = p1 + p2;
-//    cout << "p4.m_a = " << p4.m_a << endl;
-
-//    Person p5 = operator+(p1,p2);
-//    cout << "p4.m_a = " << p5.m_a << endl;
-
-//    // 成员函数、全局函数重载
-//    // 原式分别为 p1.operator+(p2)   operator+(p1,p2)
-//    Person p6 = p1 + p2;
-//    cout << "p6.m_a = " << p6.m_a << endl;
-//    cout << "p6.m_b = " << p6.m_b << endl;
-
-//    // operator+ 重载
-//    Person p7 = p1 + 11;
-//    cout << "p7.m_a = " << p7.m_a << endl;
-
-//    Person p8 = p1 - p2;
-//    cout << "p8.m_a = " << p8.m_a << endl;
-
-//    Person p9 = p1 * p2;
-//    cout << "p9.m_a = " << p9.m_a << endl;
-
-}
-
-
-/**
- * 左移运算符重载
- * @return
- */
-class Person2 {
-    friend ostream &operator<<(ostream &cout, Person2 &p);
-
-public:
-    Person2(int a, int b) {
-        m_a = a;
-        m_b = b;
-    }
-
-private:
-    int m_a;
-    int m_b;
-};
-
-// ostream 全局只能有一个，所以要通过引用的方式传递进来
-// 重载左移运算符，输出自定义数据类型
-ostream &operator<<(ostream &cout, Person2 &p) {
-    cout << "m_a = " << p.m_a << "; m_b = " << p.m_b;
-    return cout;
-}
-
-void test02() {
-
-    Person2 p(10, 10);
-
-    cout << p << " hello " << endl;
-
-}
-
-
-/**
- * 递增运算符重载
- * @return
- */
-class MyInteger {
-    // 让全局函数访问类中的私有成员变量
-    friend ostream &operator<<(ostream &cout, MyInteger myInteger);
-
-public:
-    MyInteger() {
-        m_num = 0;
-    }
-
-    // 重载前置递增
-    MyInteger &operator++() {
-        m_num++;
-        return *this;
-    }
-
-    // 重载后置递增
-    // int 代表占位参数，可以用于输出前置和后置递增
-    MyInteger operator++(int) {
-        // 记录当前结果
-        MyInteger temp = *this;
-        // 递增
-        m_num++;
-        // 将记录结果做返回
-        return temp;
-    }
-
-private:
-    int m_num;
-
-};
-
-// 先重载左移运算符
-ostream &operator<<(ostream &cout, MyInteger myInteger) {
-    cout << myInteger.m_num;
-    return cout;
-}
-
-void test03() {
-    MyInteger myInteger;
-    cout << ++myInteger << endl;
-}
-
-void test04() {
-    MyInteger myInteger;
-    cout << "myInteger = " << myInteger++ << endl;
-    cout << "myInteger = " << myInteger << endl;
-}
-
-
-/**
- * 赋值运算符重载
- */
-class Person5 {
-public:
-    Person5(int age) {
-        // new int 返回的为 int*
-        m_age = new int(age);
-    }
-
-    // 堆区开辟的空间需要手动释放
-//    ~Person5(){
-//        if (m_age != NULL){
+//
+//    Person add_person_param2(Person p) {
+//        Person temp;
+//        temp.m_a = this->m_a + p.m_a;
+//        temp.m_b = this->m_b + p.m_b;
+//        return temp;
+//    }
+//
+////    Person operator+(Person &p) {
+////        Person temp;
+////        temp.m_a = this->m_a + p.m_a;
+////        temp.m_b = this->m_b + p.m_b;
+////        return temp;
+////    }
+//
+//};
+//
+//
+//Person operator+(Person &p1, Person &p2) {
+//    Person temp;
+//    temp.m_a = p1.m_a + p2.m_a;
+//    temp.m_b = p1.m_b + p2.m_b;
+//    return temp;
+//}
+//
+//// operator+ 重载
+//Person operator+(Person &p1, int num) {
+//    Person temp;
+//    temp.m_a = p1.m_a + num;
+//    temp.m_b = p1.m_b + num;
+//    return temp;
+//}
+//
+//Person operator-(Person &p1, Person &p2) {
+//    Person temp;
+//    temp.m_a = p1.m_a - p2.m_a;
+//    temp.m_b = p1.m_b - p2.m_b;
+//    return temp;
+//}
+//
+//Person operator*(Person &p1, Person &p2) {
+//    Person temp;
+//    temp.m_a = p1.m_a * p2.m_a;
+//    temp.m_b = p1.m_b * p2.m_b;
+//    return temp;
+//}
+//
+//void test01() {
+//
+//    Person p1;
+//    p1.m_a = 10;
+//    p1.m_b = 10;
+//
+//    Person p2;
+//    p2.m_a = 10;
+//    p2.m_b = 10;
+//
+////    p1.add_person_param(p2);
+////    cout << "p1.m_a = " << p1.m_a << endl;
+////    cout << "p2.m_a = " << p2.m_a << endl;
+//
+////    p1.add_person_param2(p2);
+////    cout << "p1.m_a = " << p1.m_a << endl;
+////    cout << "p2.m_a = " << p2.m_a << endl;
+//
+////    Person p3 = p1.operator+(p2);
+////    cout << "p3.m_a = " << p3.m_a << endl;
+//
+//    // 通过成员函数重载 + 号
+////    Person p4 = p1 + p2;
+////    cout << "p4.m_a = " << p4.m_a << endl;
+//
+////    Person p5 = operator+(p1,p2);
+////    cout << "p4.m_a = " << p5.m_a << endl;
+//
+////    // 成员函数、全局函数重载
+////    // 原式分别为 p1.operator+(p2)   operator+(p1,p2)
+////    Person p6 = p1 + p2;
+////    cout << "p6.m_a = " << p6.m_a << endl;
+////    cout << "p6.m_b = " << p6.m_b << endl;
+//
+////    // operator+ 重载
+////    Person p7 = p1 + 11;
+////    cout << "p7.m_a = " << p7.m_a << endl;
+//
+////    Person p8 = p1 - p2;
+////    cout << "p8.m_a = " << p8.m_a << endl;
+//
+////    Person p9 = p1 * p2;
+////    cout << "p9.m_a = " << p9.m_a << endl;
+//
+//}
+//
+//
+///**
+// * 左移运算符重载
+// * @return
+// */
+//class Person2 {
+//    friend ostream &operator<<(ostream &cout, Person2 &p);
+//
+//public:
+//    Person2(int a, int b) {
+//        m_a = a;
+//        m_b = b;
+//    }
+//
+//private:
+//    int m_a;
+//    int m_b;
+//};
+//
+//// ostream 全局只能有一个，所以要通过引用的方式传递进来
+//// 重载左移运算符，输出自定义数据类型
+//ostream &operator<<(ostream &cout, Person2 &p) {
+//    cout << "m_a = " << p.m_a << "; m_b = " << p.m_b;
+//    return cout;
+//}
+//
+//void test02() {
+//
+//    Person2 p(10, 10);
+//
+//    cout << p << " hello " << endl;
+//
+//}
+//
+//
+///**
+// * 递增运算符重载
+// * @return
+// */
+//class MyInteger {
+//    // 让全局函数访问类中的私有成员变量
+//    friend ostream &operator<<(ostream &cout, MyInteger myInteger);
+//
+//public:
+//    MyInteger() {
+//        m_num = 0;
+//    }
+//
+//    // 重载前置递增
+//    MyInteger &operator++() {
+//        m_num++;
+//        return *this;
+//    }
+//
+//    // 重载后置递增
+//    // int 代表占位参数，可以用于输出前置和后置递增
+//    MyInteger operator++(int) {
+//        // 记录当前结果
+//        MyInteger temp = *this;
+//        // 递增
+//        m_num++;
+//        // 将记录结果做返回
+//        return temp;
+//    }
+//
+//private:
+//    int m_num;
+//
+//};
+//
+//// 先重载左移运算符
+//ostream &operator<<(ostream &cout, MyInteger myInteger) {
+//    cout << myInteger.m_num;
+//    return cout;
+//}
+//
+//void test03() {
+//    MyInteger myInteger;
+//    cout << ++myInteger << endl;
+//}
+//
+//void test04() {
+//    MyInteger myInteger;
+//    cout << "myInteger = " << myInteger++ << endl;
+//    cout << "myInteger = " << myInteger << endl;
+//}
+//
+//
+///**
+// * 赋值运算符重载
+// */
+//class Person5 {
+//public:
+//    Person5(int age) {
+//        // new int 返回的为 int*
+//        m_age = new int(age);
+//    }
+//
+//    // 堆区开辟的空间需要手动释放
+////    ~Person5(){
+////        if (m_age != NULL){
+////            delete m_age;
+////            m_age = NULL;
+////        }
+////    }
+//
+//    Person5 &operator=(Person5 &p) {
+//        if (m_age != NULL) {
 //            delete m_age;
 //            m_age = NULL;
 //        }
+//        m_age = new int(*p.m_age);
+//        return *this;
 //    }
-
-    Person5 &operator=(Person5 &p) {
-        if (m_age != NULL) {
-            delete m_age;
-            m_age = NULL;
-        }
-        m_age = new int(*p.m_age);
-        return *this;
-    }
-
-
-    int *m_age;
-};
-
-void test05() {
-    Person5 p(10);
-
-    Person5 p2(20);
-
-    p2 = p;
-
-    cout << "p.age = " << *p.m_age << endl;
-    cout << "p2.age = " << *p2.m_age << endl;
-}
-
-
-void test05_1() {
-    int a = 1;
-    int b = 2;
-    int c = 3;
-
-    c = b = a;
-
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
-    cout << "c = " << c << endl;
-
-
-}
-
-void test05_2() {
-    Person5 p1(1);
-    Person5 p2(2);
-    Person5 p3(3);
-
-    p3 = p2 = p1;
-
-    cout << "p1.age = " << *p1.m_age << endl;
-    cout << "p2.age = " << *p2.m_age << endl;
-    cout << "p3.age = " << *p3.m_age << endl;
-}
-
-
-/**
- * 重载关系运算符
- * @return
- */
-class Person6 {
-public:
-    Person6(string name, int age) {
-        m_name = name;
-        m_age = age;
-    }
-
-    // 重载 == 号
-    bool operator==(Person6 &p) {
-        if (this->m_age == p.m_age && this->m_name == p.m_name) {
-            return true;
-        }
-        return false;
-    }
-
-    // 重载 != 号
-    bool operator!=(Person6 &p) {
-        if (this->m_name != p.m_name || this->m_age != p.m_age) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    string m_name;
-    int m_age;
-
-};
-
-
-void test06() {
-    Person6 p1("zs", 10);
-    Person6 p2("ls", 20);
-    Person6 p3("zs", 10);
-
-    if (p1 == p2) {
-        cout << "相等" << endl;
-    } else {
-        cout << "不相等 " << endl;
-    }
-}
-
-void test06_2() {
-    Person6 p1("zs", 10);
-    Person6 p2("ls", 20);
-    Person6 p3("zs", 10);
-
-    if (p1 == p3) {
-        cout << "不相等" << endl;
-    } else {
-        cout << "相等 " << endl;
-    }
-}
-
-
-/**
- * 函数调用运算符重载 () --> 仿函数
- * 使用起来非常像函数调用，所以叫仿函数
- */
-// 打印输出类
-class MyPrint {
-public:
-    // 重载函数调用运算符
-    void operator()(string test) {
-        cout << test << endl;
-    }
-
-};
-
-void MyPrint2(string name) {
-    cout << name << endl;
-}
-
-void test07() {
-    MyPrint myPrint;
-    myPrint("hello world");
-
-    MyPrint2("hello2");
-}
-
-
-class MyAdd {
-public:
-    int operator()(int a, int b) {
-        return a + b;
-    }
-};
-
-void test07_2() {
-    MyAdd myAdd;
-    int res = myAdd(1, 2);
-    cout << "res = " << res << endl;
-
-    // 匿名函数对象
-    cout << MyAdd()(1,2) << endl;
-
-}
-
-
-
-
-int main() {
-
-//    test01();
-
-//    test02();
-
-//    test03();
-
-//    test04();
-
-//    test05();
-
-//    test05_1();
-
-//    test05_2();
-
-//    test06();
-
-//    test06_2();
-
-//    test07();
-
-    test07_2();
-
-
-
-}
+//
+//
+//    int *m_age;
+//};
+//
+//void test05() {
+//    Person5 p(10);
+//
+//    Person5 p2(20);
+//
+//    p2 = p;
+//
+//    cout << "p.age = " << *p.m_age << endl;
+//    cout << "p2.age = " << *p2.m_age << endl;
+//}
+//
+//
+//void test05_1() {
+//    int a = 1;
+//    int b = 2;
+//    int c = 3;
+//
+//    c = b = a;
+//
+//    cout << "a = " << a << endl;
+//    cout << "b = " << b << endl;
+//    cout << "c = " << c << endl;
+//
+//
+//}
+//
+//void test05_2() {
+//    Person5 p1(1);
+//    Person5 p2(2);
+//    Person5 p3(3);
+//
+//    p3 = p2 = p1;
+//
+//    cout << "p1.age = " << *p1.m_age << endl;
+//    cout << "p2.age = " << *p2.m_age << endl;
+//    cout << "p3.age = " << *p3.m_age << endl;
+//}
+//
+//
+///**
+// * 重载关系运算符
+// * @return
+// */
+//class Person6 {
+//public:
+//    Person6(string name, int age) {
+//        m_name = name;
+//        m_age = age;
+//    }
+//
+//    // 重载 == 号
+//    bool operator==(Person6 &p) {
+//        if (this->m_age == p.m_age && this->m_name == p.m_name) {
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    // 重载 != 号
+//    bool operator!=(Person6 &p) {
+//        if (this->m_name != p.m_name || this->m_age != p.m_age) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//    string m_name;
+//    int m_age;
+//
+//};
+//
+//
+//void test06() {
+//    Person6 p1("zs", 10);
+//    Person6 p2("ls", 20);
+//    Person6 p3("zs", 10);
+//
+//    if (p1 == p2) {
+//        cout << "相等" << endl;
+//    } else {
+//        cout << "不相等 " << endl;
+//    }
+//}
+//
+//void test06_2() {
+//    Person6 p1("zs", 10);
+//    Person6 p2("ls", 20);
+//    Person6 p3("zs", 10);
+//
+//    if (p1 == p3) {
+//        cout << "不相等" << endl;
+//    } else {
+//        cout << "相等 " << endl;
+//    }
+//}
+//
+//
+///**
+// * 函数调用运算符重载 () --> 仿函数
+// * 使用起来非常像函数调用，所以叫仿函数
+// */
+//// 打印输出类
+//class MyPrint {
+//public:
+//    // 重载函数调用运算符
+//    void operator()(string test) {
+//        cout << test << endl;
+//    }
+//
+//};
+//
+//void MyPrint2(string name) {
+//    cout << name << endl;
+//}
+//
+//void test07() {
+//    MyPrint myPrint;
+//    myPrint("hello world");
+//
+//    MyPrint2("hello2");
+//}
+//
+//
+//class MyAdd {
+//public:
+//    int operator()(int a, int b) {
+//        return a + b;
+//    }
+//};
+//
+//void test07_2() {
+//    MyAdd myAdd;
+//    int res = myAdd(1, 2);
+//    cout << "res = " << res << endl;
+//
+//    // 匿名函数对象
+//    cout << MyAdd()(1,2) << endl;
+//
+//}
+//
+//
+//
+//
+//int main() {
+//
+////    test01();
+//
+////    test02();
+//
+////    test03();
+//
+////    test04();
+//
+////    test05();
+//
+////    test05_1();
+//
+////    test05_2();
+//
+////    test06();
+//
+////    test06_2();
+//
+////    test07();
+//
+//    test07_2();
+//
+//
+//
+//}
---
+++ p84/08_extends.cpp
@@ -0,0 +1,443 @@
+#include "iostream"
+
+using namespace std;
+
+/**
+* @author: vikingar
+* @time: 2023/9/10 19:01
+* @description: 继承
+*/
+
+// java 页面
+class Java {
+public:
+    void header() {
+        cout << "首页、公开课、登录、注册...（公共头部）" << endl;
+    }
+
+    void footer() {
+        cout << "帮助中心、交流合作、站内地图...(公共底部)" << endl;
+    }
+
+    void left() {
+        cout << "Java,Python,C++...(公共分类列表)" << endl;
+    }
+
+    void content() {
+        cout << "JAVA学科视频" << endl;
+    }
+};
+
+//Python页面
+class Python {
+public:
+    void header() {
+        cout << "首页、公开课、登录、注册...（公共头部）" << endl;
+    }
+
+    void footer() {
+        cout << "帮助中心、交流合作、站内地图...(公共底部)" << endl;
+    }
+
+    void left() {
+        cout << "Java,Python,C++...(公共分类列表)" << endl;
+    }
+
+    void content() {
+        cout << "Python学科视频" << endl;
+    }
+};
+
+//C++页面
+class CPP {
+public:
+    void header() {
+        cout << "首页、公开课、登录、注册...（公共头部）" << endl;
+    }
+
+    void footer() {
+        cout << "帮助中心、交流合作、站内地图...(公共底部)" << endl;
+    }
+
+    void left() {
+        cout << "Java,Python,C++...(公共分类列表)" << endl;
+    }
+
+    void content() {
+        cout << "C++学科视频" << endl;
+    }
+};
+
+void test01() {
+
+    //Java页面
+    cout << "Java下载视频页面如下： " << endl;
+    Java ja;
+    ja.header();
+    ja.footer();
+    ja.left();
+    ja.content();
+    cout << "--------------------" << endl;
+
+    //Python页面
+    cout << "Python下载视频页面如下： " << endl;
+    Python py;
+    py.header();
+    py.footer();
+    py.left();
+    py.content();
+    cout << "--------------------" << endl;
+
+    //C++页面
+    cout << "C++下载视频页面如下： " << endl;
+    CPP cp;
+    cp.header();
+    cp.footer();
+    cp.left();
+    cp.content();
+    cout << "--------------------" << endl;
+
+
+}
+
+/**
+ * 继承的好处：==可以减少重复的代码==
+ * class A : public B;
+ * A 类称为子类 或 派生类
+ * B 类称为父类 或 基类
+ */
+// 继承实现页面
+// 公共页面类
+class BasePage {
+public:
+    void header() {
+        cout << "首页、公开课、登录、注册...（公共头部）" << endl;
+    }
+
+    void footer() {
+        cout << "帮助中心、交流合作、站内地图...(公共底部)" << endl;
+    }
+
+    void left() {
+        cout << "Java,Python,C++...(公共分类列表)" << endl;
+    }
+};
+
+class Java2 : public BasePage{
+public:
+    void content() {
+        cout << "JAVA学科视频" << endl;
+    }
+
+};
+//Python页面
+class Python2 : public BasePage
+{
+public:
+    void content()
+    {
+        cout << "Python学科视频" << endl;
+    }
+};
+//C++页面
+class CPP2 : public BasePage
+{
+public:
+    void content()
+    {
+        cout << "C++学科视频" << endl;
+    }
+};
+
+void test02(){
+
+    //Java页面
+    cout << "Java下载视频页面如下： " << endl;
+    Java2 ja;
+    ja.header();
+    ja.footer();
+    ja.left();
+    ja.content();
+    cout << "--------------------" << endl;
+
+    //Python页面
+    cout << "Python下载视频页面如下： " << endl;
+    Python2 py;
+    py.header();
+    py.footer();
+    py.left();
+    py.content();
+    cout << "--------------------" << endl;
+
+    //C++页面
+    cout << "C++下载视频页面如下： " << endl;
+    CPP2 cp;
+    cp.header();
+    cp.footer();
+    cp.left();
+    cp.content();
+
+}
+
+
+/**
+ * 继承方式
+ * public / protected / private
+ * @return
+ */
+// 公共继承
+class Father1{
+public:
+    int m_a;
+protected:
+    int m_b;
+private:
+    int m_c;
+};
+
+class Son1 : public Father1{
+public:
+    void func(){
+        m_a = 10;
+        m_b = 10;
+//        m_c = 10;
+    }
+};
+
+void test03_1() {
+    Son1 s1;
+    cout << s1.m_a << endl;
+    // son1 中 m_b 是受保护的权限，类外不能访问
+//    s1.m_b;
+}
+
+// 保护继承
+class Father2 {
+public:
+    int m_a;
+protected:
+    int m_b;
+private:
+    int m_c;
+};
+
+class Son2 : protected Father2{
+public:
+    void func(){
+        // 父类中公共成员，到子类中为保护的成员
+        m_a = 100;
+        m_b = 100;
+        // 父类中的私有成员，子类访问不到
+//        m_c = 100;
+    }
+};
+
+void test03_2() {
+    Son2 s2;
+    // 在 son2 中 m_a 和 m_b 变为保护权限，无法再类外访问
+//    s2.m_a = 1;
+//    s2.m_b = 1;
+}
+
+// 私有继承
+class Father3{
+public:
+    int m_a;
+protected:
+    int m_b;
+private:
+    int m_c;
+};
+
+class Son3 : private Father3{
+public:
+    void fun(){
+        // 父类中公共和保护的成员变量，到子类中变为私有的成员变量
+        m_a = 100;
+        m_b = 100;
+//        m_c = 100;
+    }
+};
+
+class GrandSon3 : public Son3{
+public:
+    void func(){
+//        m_a = 100;
+//        m_b = 100;
+    }
+};
+
+void test03_3(){
+    Son3 s3;
+    // 私有成员更无法在类外访问
+//    s3.m_a;
+//    s3.m_b;
+}
+
+
+/**
+ * 继承中的对象模型
+ * @return
+ */
+class Base{
+public:
+    int m_a;
+protected:
+    int m_b;
+private:
+    int m_c;
+};
+
+class SubBase : public Base{
+public:
+    int m_d;
+};
+
+void test04_4() {
+    // 父类中所有非静态的成员属性都会被子类继承下去p
+    cout << "sizeof(subBase) = " << sizeof(SubBase) << endl;
+}
+
+
+/**
+ * 继承中的构造和析构的顺序
+ * @return
+ */
+class Base2{
+public:
+    Base2(){
+        cout << "base2的构造函数" << endl;
+    }
+    ~Base2(){
+        cout << "base2的析构函数" << endl;
+    }
+};
+
+class SubBase2 : public Base2{
+public:
+    SubBase2(){
+        cout << "SubBase2的构造函数" << endl;
+    }
+    ~SubBase2(){
+        cout << "SubBase2的析构函数" << endl;
+    }
+};
+
+void test05(){
+    SubBase2 s;
+};
+
+
+/**
+ * 继承同名成员处理方式
+ * @return
+ */
+class Base3{
+public:
+    Base3(){
+        m_a = 100;
+    }
+
+    void func1(){
+        cout << "this is father " << endl;
+    }
+
+    int m_a;
+};
+
+class SubBase3 : public Base3{
+public:
+    SubBase3(){
+        m_a = 200;
+    }
+    int m_a;
+
+    void func1(){
+        cout << "this is son " << endl;
+    }
+};
+
+void test06(){
+
+//    SubBase3 s;
+//    cout << "Son s.m_a = " << s.m_a << endl;
+//
+//    // 子类访问父类中的同名成员需要加作用域
+//    cout << "Base m_a = " << s.Base3::m_a << endl;
+
+    SubBase3 s;
+//    s.func1();
+    s.Base3::func1();
+
+}
+
+
+/**
+ * 继承同名静态成员处理方式
+ * @return
+ */
+class Base4{
+public:
+    static int m_a;
+
+    static void func1(){
+        cout << "father func1 " << endl;
+    }
+};
+int Base4::m_a = 100;
+
+class SubBase4 : public Base4{
+public:
+    static int m_a;
+
+    static void func1(){
+        cout << "son func1" << endl;
+    }
+};
+int SubBase4::m_a = 200;
+
+void test07() {
+
+////     同名静态成员属性
+////     通过对象方式访问
+//    SubBase4 s;
+//    cout << "s.m_a = " << s.m_a << endl;
+//    cout << "s.Base4::m_a = " << s.Base4::m_a << endl;
+
+//    // 通过类名方式访问
+//    cout << "SubBase4::m_a = " << SubBase4::m_a << endl;
+//    // 第一个 :: 表示通过类名的方式访问，第二个 :: 表示访问父类作用域下
+//    cout << "Base4::m_a = " << SubBase4::Base4::m_a << endl;
+
+//    // 同名静态成员函数
+//    // 通过对象的方式访问
+//    SubBase4 s;
+////    s.func1();
+//    s.Base4::func1();
+
+//    // 通过类名的方式访问
+////    SubBase4::func1();
+//    SubBase4::Base4::func1();
+
+
+
+}
+
+
+
+int main() {
+
+//    test01();
+
+//    test02();
+
+//    test04_4();
+
+//    test05();
+
+//    test06();
+
+    test07();
+
+    return 0;
+}
