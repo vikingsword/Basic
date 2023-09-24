3e09dc48ca14e88bb663e3252749542941610e60
vikingar <423141759@qq.com>
2023-09-24 22:26:52 +0800

    practice

---
+++ cherno/p1_p25/headers/log.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "iostream"
+
+using namespace std;
+
+static void log(const char * message) {
+    cout << "hello" << endl;
+//    cout << message << endl;
+}
+
+
---
+++ cherno/p1_p25/log.cpp
@@ -0,0 +1,9 @@
+#include "iostream"
+#include "./headers/log.h"
+
+using namespace std;
+
+void InitLog() {
+    log("vikingar");
+}
+
